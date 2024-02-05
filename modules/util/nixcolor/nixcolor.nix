a16f9ac2e12b89b8ed87ee267ce17ddb30e6a7b9
Rasmus Enevoldsen <rasmus@enev.dk>
2024-02-05 20:48:15 +0100

    configured kde and zsh. Stated with configuring colors with nixcolors and color16

--- modules/util/nixcolor/nixcolor.nix
+++ modules/util/nixcolor/nixcolor.nix
@@ -7,8 +7,9 @@
   imports = [
     inputs.nix-colors.homeManagerModules.default
     
-    
-    ./featues/kitty.nix
+    # Templates
+    # https://github.com/chriskempson/base16-templates-source?tab=readme-ov-file
+    ./features/kitty.nix
   ];
 
   # This is the link to all the style guides
