fd75d4335a3bbc54288da99b22d7dafcc062b2c6
Rasmus Enevoldsen <rasmus@enev.dk>
2024-02-13 17:37:28 +0100

    fix: add Neovim EDITOR, move sessionvars NIXLANG
    I added Neovim as the editor on all sessions.
    I moved session variables from strings to home-manager options.

--- modules/home-manager/zsh/zshenv.nix
+++ modules/home-manager/zsh/zshenv.nix
@@ -12,19 +12,30 @@
     stateHome = "${config.home.homeDirectory}/.local/state";
   };
   
+  # Environment Variables to always set at login
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+
+    # gnupg as ssh-agent
+    SSH_AUTH_SOCK = "$(gpgconf --list-dirs agent-ssh-socket)";
+
+    # flatpak
+    XDG_DATA_DIRS = "$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share";
+  };
+
   # Environment Variables in the .zshenv file
-  programs.zsh.envExtra = ''
-      export COWPATH="$XDG_DATA_HOME/cowsay/"
-
-      # flatpak
-      export XDG_DATA_DIRS=$XDG_DATA_DIRS:/usr/share:/var/lib/flatpak/exports/share:$HOME/.local/share/flatpak/exports/share
-
-      # export SSH_AUTH_SOCK=$XDG_RUNTIME_DIR/ssh-agent.socket
-
-      # gnupg as ssh-agent
-      export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)" # set SSH_AUTH_SOCK
-      gpg-connect-agent updatestartuptty /bye > /dev/null # help pgp find user tty for password prompts
-
-
+  programs.zsh = {
+    initExtra = ''
+    gpg-connect-agent updatestartuptty /bye > /dev/null # help pgp find user tty for password prompts
     '';
+    sessionVariables = {
+      # flatpak
+
+      COWPATH = "$XDG_DATA_HOME/cowsay/";
+    };
+
+    envExtra = ''
+    '';
+  };
 }
