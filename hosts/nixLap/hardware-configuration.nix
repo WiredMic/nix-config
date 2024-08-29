106aa0d299713ebe7a863acc66918e97d0686609
Rasmus Enevoldsen <rasmus@enev.dk>
2024-08-29 14:21:47 +0200

    fix: add nfs NAS mount to hardware file.
    This is more stable

--- hosts/nixLap/hardware-configuration.nix
+++ hosts/nixLap/hardware-configuration.nix
@@ -4,28 +4,32 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules =
+    [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/6f6037e1-be00-4b82-a50b-ecd66cf8e476";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/6f6037e1-be00-4b82-a50b-ecd66cf8e476";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/21AD-B392";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/21AD-B392";
+    fsType = "vfat";
+  };
+
+  fileSystems."/mnt/share" = {
+    device = "192.168.86.101:/mnt/ZPOOL0/share";
+    fsType = "nfs";
+    options = [ "x-systemd.automount" "noauto" ];
+  };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/b9edbf58-f8fa-489f-a5ab-80741afe9dcb"; }
-    ];
+    [{ device = "/dev/disk/by-uuid/b9edbf58-f8fa-489f-a5ab-80741afe9dcb"; }];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
@@ -36,5 +40,6 @@
   # networking.interfaces.wlp7s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
