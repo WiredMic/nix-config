0665fd80b218295fe5f9278f0f4aff6820a2a3e8
Rasmus Enevoldsen <rasmus@enev.dk>
2025-01-24 17:30:50 +0100

    feat: change octave history file location

--- home/rasmus/common/optional/octave/octave.nix
+++ home/rasmus/common/optional/octave/octave.nix
@@ -18,5 +18,16 @@
         ]))
       ghostscript
     ];
+
+    home.sessionVariables = {
+      OCTAVE_HISTFILE = "${config.xdg.cacheHome}/octave-hsts";
+      OCTAVE_SITE_INITFILE = "${config.xdg.configHome}/octave/octaverc";
+    };
+
+    xdg.configFile."octave/octaverc".text = ''
+      #source /usr/share/octave/site/m/startup/octaverc;
+      # pkg prefix ~/.local/share/octave/packages ~/.local/share/octave/packages;
+      # pkg local_list ${config.home.homeDirectory}/.local/share/octave/octave_packages;
+    '';
   };
 }
