d0b3902b39708c53a0da1534f67868dcdb556af2
Rasmus Enevoldsen <rasmus@enev.dk>
2025-03-02 19:53:00 +0100

    fix: disable home row modes

--- hosts/common/optional/kanata.nix
+++ hosts/common/optional/kanata.nix
@@ -22,7 +22,7 @@
           extraDefCfg = "process-unmapped-keys yes";
           config = ''
             (defsrc
-             caps a s d f j k l ;
+             caps ;; a s d f j k l ;
             )
             (defvar
              tap-time 150
@@ -30,18 +30,18 @@
             )
             (defalias
              caps (tap-hold 150 200 esc lctl)
-             a (tap-hold $tap-time $hold-time a lmet)
-             s (tap-hold $tap-time $hold-time s lalt)
-             d (tap-hold $tap-time $hold-time d lsft)
-             f (tap-hold $tap-time $hold-time f lctl)
-             j (tap-hold $tap-time $hold-time j rctl)
-             k (tap-hold $tap-time $hold-time k rsft)
-             l (tap-hold $tap-time $hold-time l ralt)
-             ; (tap-hold $tap-time $hold-time ; rmet)
+             ;; a (tap-hold $tap-time $hold-time a lmet)
+             ;; s (tap-hold $tap-time $hold-time s lalt)
+             ;; d (tap-hold $tap-time $hold-time d lsft)
+             ;; f (tap-hold $tap-time $hold-time f lctl)
+             ;; j (tap-hold $tap-time $hold-time j rctl)
+             ;; k (tap-hold $tap-time $hold-time k rsft)
+             ;; l (tap-hold $tap-time $hold-time l ralt)
+             ;; ; (tap-hold $tap-time $hold-time ; rmet)
             )
 
             (deflayer base
-             @caps @a  @s  @d  @f  @j  @k  @l  @;
+             @caps ;; @a  @s  @d  @f  @j  @k  @l  @;
             )
           '';
         };
