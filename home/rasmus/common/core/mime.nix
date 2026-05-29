deeb1ed6b3ed88769b64b9b2865bfe92598a5869
Rasmus Enevoldsen <rasmus@enev.dk>
2026-05-29 17:35:33 +0200

    add okular as pdf reader

--- home/rasmus/common/core/mime.nix
+++ home/rasmus/common/core/mime.nix
@@ -1,4 +1,9 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
 
@@ -7,17 +12,15 @@
   };
 
   config = lib.mkIf config.my.mime.enable {
-    # xdg.mimeApps.enable = false;
-
-    # home.xdg.dataFile."mime/application/ltspice.xml".text = ''
-    #   <?xml version="1.0" encoding="utf-8"?>
-    #   <mime-info xmlns="http://www.freedesktop.org/standards/shared-mime-info">
-    #   <mime-type type="application/vnd.jgraph.mxfile">
-    #     <glob pattern="*.asc"/>
-    #       <comment>LTSpice file</comment>
-    #     <icon="/home/rasmus/.var/app/com.usebottles.bottles/data/bottles/bottles/LTspice/icons/LTspice.png" />
-    #   </mime-type>
-    #   </mime-info>
-    # '';
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/ftp" = "firefox.desktop";
+        "text/html" = "firefox.desktop";
+        "application/pdf" = "org.kde.okular.desktop";
+      };
+    };
   };
 }
