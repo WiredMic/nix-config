0d170288bdb633bc1f620b2aa6626193a59b9f21
Rasmus Enevoldsen <rasmus@enev.dk>
2026-06-03 20:50:01 +0200

    add reminder of openldap

--- hosts/nixLap/configuration.nix
+++ hosts/nixLap/configuration.nix
@@ -45,6 +45,7 @@
       #   });
       # })
 
+      # https://github.com/NixOS/nixpkgs/issues/514113
       (_: prev: {
         openldap = prev.openldap.overrideAttrs {
           doCheck = !prev.stdenv.hostPlatform.isi686;
