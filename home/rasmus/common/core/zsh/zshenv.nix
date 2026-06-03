87f1fdd26c41c35aece450590ed6f02e4b5b559d
Rasmus Enevoldsen <rasmus@enev.dk>
2026-06-03 21:31:21 +0200

    remove extra /

--- home/rasmus/common/core/zsh/zshenv.nix
+++ home/rasmus/common/core/zsh/zshenv.nix
@@ -11,8 +11,8 @@
   # XDG
   xdg = {
     cacheHome = "${config.home.homeDirectory}/.cache";
-    configHome = "${config.home.homeDirectory}/.config/";
-    dataHome = "${config.home.homeDirectory}/.local/share/";
+    configHome = "${config.home.homeDirectory}/.config";
+    dataHome = "${config.home.homeDirectory}/.local/share";
     stateHome = "${config.home.homeDirectory}/.local/state";
   };
 
